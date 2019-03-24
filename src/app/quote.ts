a41f27ab1775da29ea35553951c5f7e06bf671a9
Elle <mitchellemakini15@gmail.com>
2019-03-24 21:13:28 +0300

    added pipes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,12 @@
 export class Quote {
     public date:Date;
+    public showDetails:Boolean;
     public upvote:number;
     public downvote:number;
     constructor(public quote:string,public author:string,public publisher:string){
         this.date=new Date();
         this.upvote=0;
         this.downvote=0;
+        this.showDetails=false;
     }
 }
