bd24dc78db8c051434d7d0d063eb864cc73abbca
Elle <mitchellemakini15@gmail.com>
2019-03-26 13:13:26 +0300

    added css

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {Quote} from '../quote'
+import { Component, OnInit, Input } from "@angular/core";
+import { Quote } from "../quote";
 
 @Component({
-  selector: 'app-quote-details',
-  templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.css']
+  selector: "app-quote-details",
+  templateUrl: "./quote-details.component.html",
+  styleUrls: ["./quote-details.component.css"]
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() quote: Quote;
 
-  @Input() quote:Quote;
-  constructor() { }
+  showDetails: boolean = true;
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  toggleCollapsed(i) {
+    this.showDetails = !this.showDetails;
   }
-
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,39 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import {Quote} from '../quote'
+import { Component, OnInit } from "@angular/core";
+import { Quote } from "../quote";
 
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  selector: "app-quote",
+  templateUrl: "./quote.component.html",
+  styleUrls: ["./quote.component.css"]
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote('Reality is an illusion','NKK','Elle Makini'),
-    new Quote('Forever is overated', 'Elle Makini','Elle Makini'),
-    new Quote('Promises are cheap','Lydia Makini','Elle Makini'),
-  ]
-  constructor() { }
-  
+    new Quote("Reality is an illusion", "NKK", "Elle Makini"),
+    new Quote("Forever is overated", "Elle Makini", "Elle Makini"),
+    new Quote("Promises are cheap", "Lydia Makini", "Elle Makini")
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  addQuote(quote) {
+    this.quotes.unshift(quote);
+    console.log(quote);
   }
 
-  addQuote(quote){
-    this.quotes.push(quote)
-    console.log(quote)
+  upVote(i) {
+    this.quotes[i].upvote += 1;
   }
- 
-   upVote(i){
-   this.quotes[i].upvote +=1;
-   }
-   downVote(i){
-     this.quotes[i].downvote +=1
-   }
-   deleteQuote(i){
-     this.quotes.splice(i,1)
-   }
-   showDetails(i) {
-     this.showDetails[i] != false;
-
-   }
+  downVote(i) {
+    this.quotes[i].downvote += 1;
+  }
+  deleteQuote(i) {
+    this.quotes.splice(i, 1);
+  }
 }
